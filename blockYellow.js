5c645d60ef9e77b7e04982010bbd18e42cb10901
dev31agarwal <73831623+dev31agarwal@users.noreply.github.com>
2021-04-26 19:23:51 +0530

    Add files via upload

---
+++ blockYellow.js
@@ -0,0 +1,37 @@
+class BlockYellow{
+    constructor(x, y, width, height){
+
+        var options= {
+            isStatic: false,
+        }
+
+        this.body= Bodies.rectangle(x, y, width, height, options);
+        World.add(world, this.body);
+        this.width=width;
+        this.height=height;
+        this.visibility=255
+    }
+
+    display(){
+
+        var pos=this.body.position;
+
+        if(this.body.speed>10){
+            push();
+            this.visibility=this.visibility-5
+            tint(255, this.visibility);
+            World.remove(world, this.body);
+            pop();
+        }
+
+        else{
+            fill("yellow");
+            rect(pos.x, pos.y, this.width, this.height); 
+        }
+
+        if(this.visibility<255&&this.visibility>0){
+           
+            score=score+0.1
+        }
+    }
+}
